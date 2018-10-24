e63b1e4067329159b6969135100dd16c35fe8216
rguevarasa <guevara080106f>
2018-10-24 17:28:49 -0500

    commit RGS asociar suministro

---
+++ app/src/main/java/com/example/delawareperu/dtl_and/AsociarSuministro.java
@@ -0,0 +1,14 @@
+package com.example.delawareperu.dtl_and;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AsociarSuministro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_asociar_suministro);
+
+    }
+}
